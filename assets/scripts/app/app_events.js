4091a8fad357ada24be48eccc957327da7ca6da5
MBKeeler <mark.keeler@comcast.net>
2017-12-20 14:02:25 -0500

    Adds code for adding a beverage
    Adds code to enable adding a beverage from the UI.
    Also renames navbar to authbar
    
    Next step will be to add navigation to get to the add beverage form.
    The rename of the navbar to authbar is in preparation for the next
    commit.

--- assets/scripts/app/app_events.js
+++ assets/scripts/app/app_events.js
@@ -1,11 +1,21 @@
 'use strict'
+
 const getFormFields = require(`../../../lib/get-form-fields`)
 const store = require('../store')
-const api = require('./app_api')
-const ui = require('./app_ui')
+const app_api = require('./app_api')
+const app_ui = require('./app_ui')
 
+const onAddBeverage = function (event) {
+  const data = getFormFields(this)
+  event.preventDefault()
+  console.log('onAddBeverage called with data:', data)
+  app_api.addBeverage(data)
+    .then(app_ui.addBeverageSuccess)
+    .catch(app_ui.addBeverageFailure)
+}
 
 const addHandlers = function () {
+  $('#add_bevarage_form').on('submit', onAddBeverage)
 }
 
 module.exports = {
--- assets/scripts/app/app_ui.js
+++ assets/scripts/app/app_ui.js
@@ -1,6 +1,16 @@
 'use strict'
 const store = require('../store')
 
-module.exports = {
+const addBeverageSuccess = function () {
+  $('#messages').show().html('Beverage was added successfully').fadeOut(8000)
+  $(':input', '#enter-player').val('')
+}
+const addBeverageFailure = function (error) {
+  console.error('enterPlayer failed: ', error)
+  $('#messages').show().html('There was a problem adding your beverage.').fadeOut(8000)
+}
 
+module.exports = {
+  addBeverageSuccess,
+  addBeverageFailure
 }
